9db0429f0298943641a3da0c290f69592a215097
Elvin Bernabe <bernabeelvin@email.com>
2026-02-26 19:16:58 +0800

    Fix DB config for production and local

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 // db.js
-const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config({ path: './.env' });
+
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
-  port: 3306
+  port: process.env.MYSQLPORT,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : undefined
 });
 
 db.connect((err) => {
